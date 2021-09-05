c7a8c8466fe825aed2807aa1b5bc7c95c152bf40
lzp <375849802@qq.com>
2021-09-05 21:44:06 +0800

    缓存系统常量数据， 检测当天是否可以签到

--- src/main/java/com/example/emos/wx/db/dao/TbCheckinDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbCheckinDao.java
@@ -3,7 +3,10 @@ package com.example.emos.wx.db.dao;
 import com.example.emos.wx.db.pojo.TbCheckin;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
 @Mapper
 public interface TbCheckinDao {
+    public  Integer haveCheckin(HashMap parm);
 
 }
